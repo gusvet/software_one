864a20530f74dfba4dde48175e320d50da46c14c
gusvet <gusvet2001@yahoo.com>
2017-05-23 20:35:23 -0400

    Add files via upload

---
+++ myproject.cpp
@@ -0,0 +1,24 @@
+//============================================================================
+// Name        : myproject.cpp
+// Author      : me
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int i=0;
+  int j=1;
+  int x=3;
+
+  int total = 0;
+  total = i + j;
+	// prints !!!Hello World!!!
+	cout << "this is a program" << endl;
+	cout << "total = " << total << endl;
+	return 0;
+}
